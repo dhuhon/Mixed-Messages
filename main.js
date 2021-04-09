8afdbadb01a73ec42b5c856420e708f5345e370c
Dhuhon <dhuhon@gmail.com>
2021-04-08 20:25:06 -0500

    Final update, removed tweets

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-let  tweets = ["Just did my own taxes, I should be in jail by friday","Pollen still coming out during a global pandemic??? Bitch read the room","This dog hit the “woah” better than some of you","Time to go take a stupid little fucking afternoon stroll","one time i was my college’s mascot for a basketball game. emphasis on one time"];
-let tweets2 = ["Work : Your time off request has been denied Me","I just went outside and lemme just say, THAT SUN HIT DIFFERENT WHEN ITS ILLEGAL","I AM HOWLING"];
-let tweets3 = ["guys be havin 25 roommates and want u to come over im staying home brockhampton","I told my man I wanted to go to a restaurant where they cook infront of you so he brought me to subway","our kids: what happened in 2020 us:"];
 
 let  nouns = ["The Boy","The Girl","The Doctor","The Dog","My Dad"];
 let verbs = ["was driving","was eating","was playing"];
@@ -8,10 +5,7 @@ let adjective = ["a red car","a pink elephant","a tall chicken","a small robot"]
 
 
 
-
-
-let nested = [tweets,tweets2,tweets3];
-let nested2 =[nouns,verbs,adjective];
+let nested =[nouns,verbs,adjective];
 function random(tweet){
    return Math.floor(Math.random()* tweet.length);
 }
@@ -33,4 +27,4 @@ function generatetweet(nested){
 
 
 
-console.log(generatetweet(nested2));
+console.log(generatetweet(nested));
